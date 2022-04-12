6e5e74034d979f8d67cfd830582bf1ef19c29c99
RaulMOAB <raulmontoroabaijon@gmail.com>
2022-04-12 10:24:59 +0200

    PokemonGo

--- pokemonGo/src/DAO/PokemonDAO.java
+++ pokemonGo/src/DAO/PokemonDAO.java
@@ -4,10 +4,33 @@
  */
 package DAO;
 
+import Objects.Pokemon;
+import java.util.ArrayList;
+
 /**
  *
  * @author alumne
  */
 public class PokemonDAO implements Basic_operations{
+
+    @Override
+    public boolean catchPokemon(Pokemon obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Pokemon> displayBag() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void transferPokemon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getTransferedPokemon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
