e71df8d811fcf43cf2d9d3b89327a86b57a32139
RaulMOAB <raulmontoroabaijon@gmail.com>
2022-04-12 17:39:44 +0200

    PokemonGo

--- pokemonGo/src/Persistence/FilePersistence.java
+++ pokemonGo/src/Persistence/FilePersistence.java
@@ -4,10 +4,37 @@
  */
 package Persistence;
 
+import Objects.Pokemon;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 /**
  *
  * @author Raul
  */
 public class FilePersistence {
+    private final static String file_name;
     
+    public static boolean saveItems(ArrayList<Pokemon> bag) throws FileNotFoundException, IOException{
+        FileOutputStream write = null;
+        write = new FileOutputStream(file_name);
+        ObjectOutputStream StreamData = new ObjectOutputStream(write);
+        StreamData.writeObject(bag);
+        
+        return true;
+    }
+    
+    public static ArrayList<Pokemon> readItems(ArrayList<Pokemon> bag) throws FileNotFoundException, IOException, ClassNotFoundException{
+        FileInputStream file = new FileInputStream(file_name);
+        ObjectInputStream StreamData = new ObjectInputStream(file);
+        
+        ArrayList<Pokemon> read = (ArrayList<Pokemon>) StreamData.readObject();
+        
+        return read;
+    }
 }
